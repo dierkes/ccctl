3769ef2b1cc12afc36d6206ce8e3a76519e6fec7
Frank Dierkes <dierkes@ifr.ing.tu-bs.de>
2015-03-17 18:36:27 +0100

    Removed emplace methods and stuff that was commented out

--- include/ccc/pod_vector.h
+++ include/ccc/pod_vector.h
@@ -8,8 +8,8 @@
  *
  */
 
-#ifndef CCC_POD_STACK_H_
-#define CCC_POD_STACK_H_
+#ifndef CCC_POD_VECTOR_H_
+#define CCC_POD_VECTOR_H_
 
 #include <ciso646>
 #include <cstddef>
@@ -82,7 +82,6 @@ struct PODVector
     void assign(size_type Count, const value_type& Value)
     {
         clear();
-//        std::fill(begin(), begin() + Count, Value);
         m_Storage.construct_and_assign(0, Count, Value);
         m_End = m_End + Count;
     }
@@ -91,7 +90,6 @@ struct PODVector
     void assign(IteratorType First, IteratorType Last)
     {
         clear();
-//        std::copy(First, Last, begin());
         m_Storage.construct_and_assign(0, First, Last);
         m_End = m_End + std::distance(First, Last);
     }
@@ -208,35 +206,6 @@ struct PODVector
         m_End = size_type();
     }
 
-    /**
-     * According to the standard, the push and pop methods have no return values.
-     * Further, the methods may throw exceptions (e.g., std::bad_alloc if an allocation fails).
-     *
-     * We ...
-     */
-
-//     A vector, as defined by the standard, does not have the methods push_front and pop_front.
-//    void push_front(const_reference Value)
-//    {
-//        if (size() < Capacity)
-//        {
-//            if (UseRawMemOps)
-//            {
-//                std::memmove(data(1), data(0), size() * sizeof(value_type));
-//            }
-//            else
-//            {
-//                std::copy_backward(begin(), end(), end() + 1);
-//            }
-//            m_End = m_End + 1;
-//            m_Array[m_Begin] = Value;
-//        }
-//        else
-//        {
-//            throw std::bad_alloc();
-//        }
-//    }
-
     void push_back(const_reference Value)
     {
         if (size() < Capacity)
@@ -250,64 +219,10 @@ struct PODVector
         }
     }
 
-//    void emplace_front()
-//    {
-//        if (size() < Capacity)
-//        {
-//            if (UseRawMemOps)
-//            {
-//                std::memmove(data(1), data(0), size() * sizeof(value_type));
-//            }
-//            else
-//            {
-//                std::copy_backward(begin(), end(), end() + 1);
-//            }
-//            m_End = m_End + 1;
-//            m_Array[m_Begin] = value_type();
-//        }
-//        else
-//        {
-//            throw std::bad_alloc();
-//        }
-//    }
-
-    void emplace_back()
-    {
-        if (size() < Capacity)
-        {
-            m_Storage.construct_default(m_End);
-            m_End = m_End + 1;
-        }
-        else
-        {
-            throw std::bad_alloc();
-        }
-    }
-
-//    void pop_front()
-//    {
-//        if (not empty())
-//        {
-//            // ToDo: Do we have to destroy the element?
-//            m_Array[m_Begin] = value_type(); // destroy element by overwriting
-//            if (UseRawMemOps)
-//            {
-//                std::memmove(data(0), data(1), (size() - 1) * sizeof(value_type));
-//            }
-//            else
-//            {
-//                std::copy(begin() + 1, end(), begin());
-//            }
-//            m_End = m_End - 1;
-//        }
-//    }
-
     void pop_back()
     {
         if (not empty())
         {
-            // ToDo: Do we have to destroy the element?
-//            m_Array[m_End - 1] = value_type(); // destroy element by overwriting
             m_Storage.destroy(m_End - 1);
             m_End = m_End - 1;
         }
@@ -318,17 +233,10 @@ struct PODVector
         return insert(iterator(ccc::addressof(m_Storage[Position])), Value);
     }
 
-    /**
-     *
-     * Note:
-     */
     iterator insert(iterator Position, const_reference Value)
     {
-        if (size() < Capacity) // rules out case LogicalBegin == LogicalEnd
+        if (size() < Capacity)
         {
-            // Do we need to construct the element to which we shift the other ones?
-            // Well, the object at Position is already constructed, so probably yes. Then the
-            // behavior is well defined, as all elements are constructed
             m_Storage.construct_default(m_End);
             if (UseRawMemOps)
             {
@@ -396,40 +304,10 @@ struct PODVector
         }
     }
 
-    // emplace is a bad idea, if we have no default constructor
-    iterator emplace(iterator Position)
-    {
-        if (size() < Capacity) // rules out case LogicalBegin == LogicalEnd
-        {
-            m_Storage.construct_default(m_End);
-            if (UseRawMemOps)
-            {
-                std::memmove(Position + 1, Position, (end() - Position) * sizeof(value_type));
-            }
-            else
-            {
-                std::copy_backward(Position, end(), end() + 1);
-            }
-            m_End = m_End + 1;
-            *Position = value_type();
-            return Position;
-        }
-        else
-        {
-            throw std::bad_alloc();
-        }
-    }
-
-    /**
-     *
-     * Note: According to the standard, passing an invalid iterator causes undefinied behavior.
-     */
     iterator erase(iterator Position)
     {
         if (Position < end())
         {
-            // (*Position).~T(); // call destructor of the object at Position (might not work for basic types)
-//            *Position = value_type();
             if (UseRawMemOps)
             {
                 std::memmove(Position, Position + 1, (end() - Position + 1) * sizeof(value_type));
@@ -467,4 +345,4 @@ struct PODVector
 
 }
 
-#endif /* CCC_POD_STACK_H_ */
+#endif /* CCC_POD_VECTOR_H_ */
