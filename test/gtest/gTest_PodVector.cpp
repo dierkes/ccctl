21728520e8a34b5051bd23467db380491817139c
Frank Dierkes <dierkes@ifr.ing.tu-bs.de>
2015-12-03 18:52:24 +0100

    Fixed alignment in some test cases; Added generic layout tests

--- test/gtest/gTest_PodVector.cpp
+++ test/gtest/gTest_PodVector.cpp
@@ -68,10 +68,10 @@ typedef ccc::PodVector<ccc_test::Pod<1, 1>, uint8_t, 2, 4> PodVector_1_1_1_2_4;
 typedef ccc::PodVector<ccc_test::Pod<1, 1>, uint8_t, 2, 8> PodVector_1_1_1_2_8;
 typedef ccc::PodVector<ccc_test::Pod<1, 1>, uint8_t, 2, 16> PodVector_1_1_1_2_16;
 
-typedef ccc::PodVector<ccc_test::Pod<2, 2>, uint8_t, 2, 1> PodVector_2_2_1_2_1;
-typedef ccc::PodVector<ccc_test::Pod<4, 4>, uint8_t, 2, 1> PodVector_4_4_1_2_1;
-typedef ccc::PodVector<ccc_test::Pod<8, 8>, uint8_t, 2, 1> PodVector_8_8_1_2_1;
-typedef ccc::PodVector<ccc_test::Pod<16, 16>, uint8_t, 2, 1> PodVector_16_16_1_2_1;
+typedef ccc::PodVector<ccc_test::Pod<2, 2>, uint8_t, 2, 2> PodVector_2_2_1_2_2;
+typedef ccc::PodVector<ccc_test::Pod<4, 4>, uint8_t, 2, 4> PodVector_4_4_1_2_4;
+typedef ccc::PodVector<ccc_test::Pod<8, 8>, uint8_t, 2, 8> PodVector_8_8_1_2_8;
+typedef ccc::PodVector<ccc_test::Pod<16, 16>, uint8_t, 2, 16> PodVector_16_16_1_2_16;
 
 typedef ccc::PodVector<ccc_test::Pod<2, 1>, uint8_t, 2, 1> PodVector_2_1_1_2_1;
 typedef ccc::PodVector<ccc_test::Pod<4, 1>, uint8_t, 2, 1> PodVector_4_1_1_2_1;
@@ -87,10 +87,10 @@ TEST(PodVector, ConsistentSize)
     EXPECT_EQ(16, sizeof(PodVector_1_1_1_2_8));
     EXPECT_EQ(32, sizeof(PodVector_1_1_1_2_16));
 
-    EXPECT_EQ(6, sizeof(PodVector_2_2_1_2_1));
-    EXPECT_EQ(12, sizeof(PodVector_4_4_1_2_1));
-    EXPECT_EQ(24, sizeof(PodVector_8_8_1_2_1));
-    EXPECT_EQ(48, sizeof(PodVector_16_16_1_2_1));
+    EXPECT_EQ(6, sizeof(PodVector_2_2_1_2_2));
+    EXPECT_EQ(12, sizeof(PodVector_4_4_1_2_4));
+    EXPECT_EQ(24, sizeof(PodVector_8_8_1_2_8));
+    EXPECT_EQ(48, sizeof(PodVector_16_16_1_2_16));
 
     EXPECT_EQ(5, sizeof(PodVector_2_1_1_2_1));
     EXPECT_EQ(9, sizeof(PodVector_4_1_1_2_1));
@@ -107,10 +107,10 @@ TEST(PodVector, ConsistentAlignment)
     EXPECT_EQ(8, CCC_ALIGNOF(PodVector_1_1_1_2_8));
     EXPECT_EQ(16, CCC_ALIGNOF(PodVector_1_1_1_2_16));
 
-    EXPECT_EQ(2, CCC_ALIGNOF(PodVector_2_2_1_2_1));
-    EXPECT_EQ(4, CCC_ALIGNOF(PodVector_4_4_1_2_1));
-    EXPECT_EQ(8, CCC_ALIGNOF(PodVector_8_8_1_2_1));
-    EXPECT_EQ(16, CCC_ALIGNOF(PodVector_16_16_1_2_1));
+    EXPECT_EQ(2, CCC_ALIGNOF(PodVector_2_2_1_2_2));
+    EXPECT_EQ(4, CCC_ALIGNOF(PodVector_4_4_1_2_4));
+    EXPECT_EQ(8, CCC_ALIGNOF(PodVector_8_8_1_2_8));
+    EXPECT_EQ(16, CCC_ALIGNOF(PodVector_16_16_1_2_16));
 
     EXPECT_EQ(1, CCC_ALIGNOF(PodVector_2_1_1_2_1));
     EXPECT_EQ(1, CCC_ALIGNOF(PodVector_4_1_1_2_1));
@@ -132,14 +132,14 @@ TEST(PodVector, ConsistentLayout)
     EXPECT_EQ(16, CCC_OFFSETOF(PodVector_1_1_1_2_16, m_Storage[0]));
     EXPECT_EQ(17, CCC_OFFSETOF(PodVector_1_1_1_2_16, m_Storage[1]));
 
-    EXPECT_EQ(2, CCC_OFFSETOF(PodVector_2_2_1_2_1, m_Storage[0]));
-    EXPECT_EQ(4, CCC_OFFSETOF(PodVector_2_2_1_2_1, m_Storage[1]));
-    EXPECT_EQ(4, CCC_OFFSETOF(PodVector_4_4_1_2_1, m_Storage[0]));
-    EXPECT_EQ(8, CCC_OFFSETOF(PodVector_4_4_1_2_1, m_Storage[1]));
-    EXPECT_EQ(8, CCC_OFFSETOF(PodVector_8_8_1_2_1, m_Storage[0]));
-    EXPECT_EQ(16, CCC_OFFSETOF(PodVector_8_8_1_2_1, m_Storage[1]));
-    EXPECT_EQ(16, CCC_OFFSETOF(PodVector_16_16_1_2_1, m_Storage[0]));
-    EXPECT_EQ(32, CCC_OFFSETOF(PodVector_16_16_1_2_1, m_Storage[1]));
+    EXPECT_EQ(2, CCC_OFFSETOF(PodVector_2_2_1_2_2, m_Storage[0]));
+    EXPECT_EQ(4, CCC_OFFSETOF(PodVector_2_2_1_2_2, m_Storage[1]));
+    EXPECT_EQ(4, CCC_OFFSETOF(PodVector_4_4_1_2_4, m_Storage[0]));
+    EXPECT_EQ(8, CCC_OFFSETOF(PodVector_4_4_1_2_4, m_Storage[1]));
+    EXPECT_EQ(8, CCC_OFFSETOF(PodVector_8_8_1_2_8, m_Storage[0]));
+    EXPECT_EQ(16, CCC_OFFSETOF(PodVector_8_8_1_2_8, m_Storage[1]));
+    EXPECT_EQ(16, CCC_OFFSETOF(PodVector_16_16_1_2_16, m_Storage[0]));
+    EXPECT_EQ(32, CCC_OFFSETOF(PodVector_16_16_1_2_16, m_Storage[1]));
 
     EXPECT_EQ(1, CCC_OFFSETOF(PodVector_2_1_1_2_1, m_Storage[0]));
     EXPECT_EQ(3, CCC_OFFSETOF(PodVector_2_1_1_2_1, m_Storage[1]));
@@ -150,3 +150,73 @@ TEST(PodVector, ConsistentLayout)
     EXPECT_EQ(1, CCC_OFFSETOF(PodVector_16_1_1_2_1, m_Storage[0]));
     EXPECT_EQ(17, CCC_OFFSETOF(PodVector_16_1_1_2_1, m_Storage[1]));
 }
+
+template <typename ContainerType, uint64_t Capacity, uint64_t Alignment>
+struct LayoutTraits
+{
+    typedef typename ContainerType container_type;
+    typedef typename ContainerType::value_type value_type;
+    typedef typename ContainerType::size_type size_type;
+
+    static uint64_t ExpectedAlignment()
+    {
+        return std::max(Alignment, static_cast<uint64_t>(std::max(CCC_ALIGNOF(value_type), CCC_ALIGNOF(size_type))));
+    }
+
+    static uint64_t ExpectedOffsetStorage(uint64_t i)
+    {
+        return std::max(Alignment, static_cast<uint64_t>(std::max(CCC_ALIGNOF(value_type), sizeof(size_type)))) + i * sizeof(value_type);
+    }
+
+    static uint64_t ExpectedSize()
+    {
+        uint64_t SizeOfDataArray = Capacity * sizeof(value_type);
+        uint64_t ExpectedPadding = SizeOfDataArray % Alignment ? Alignment - (SizeOfDataArray % Alignment) : 0;
+        return ExpectedOffsetStorage(0) + SizeOfDataArray + ExpectedPadding;
+    }
+};
+
+template<typename T>
+class ConsistentLayoutTest: public ::testing::Test
+{
+protected:
+
+};
+
+TYPED_TEST_CASE_P(ConsistentLayoutTest);
+
+TYPED_TEST_P(ConsistentLayoutTest, ConsistentSize)
+{
+    EXPECT_EQ(TypeParam::ExpectedSize(), sizeof(TypeParam::container_type));
+}
+
+TYPED_TEST_P(ConsistentLayoutTest, ConsistentAlignment)
+{
+    EXPECT_EQ(TypeParam::ExpectedAlignment(), CCC_ALIGNOF(TypeParam::container_type));
+}
+
+TYPED_TEST_P(ConsistentLayoutTest, ConsistentLayout)
+{
+    typedef typename TypeParam::container_type container_type;
+    EXPECT_EQ(TypeParam::ExpectedOffsetStorage(0), CCC_OFFSETOF(container_type, m_Storage[0]));
+    EXPECT_EQ(TypeParam::ExpectedOffsetStorage(1), CCC_OFFSETOF(container_type, m_Storage[1]));
+}
+
+REGISTER_TYPED_TEST_CASE_P(ConsistentLayoutTest, ConsistentSize, ConsistentAlignment, ConsistentLayout);
+
+typedef ::testing::Types<
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<1, 1>, uint8_t, 10, 1>, 10, 1>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<1, 1>, uint8_t, 10, 2>, 10, 2>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<1, 1>, uint8_t, 10, 4>, 10, 4>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<1, 1>, uint8_t, 10, 8>, 10, 8>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<1, 1>, uint8_t, 10, 16>, 10, 16>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<2, 2>, uint8_t, 10, 2>, 10, 2>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<4, 4>, uint8_t, 10, 4>, 10, 4>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<8, 8>, uint8_t, 10, 8>, 10, 8>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<16, 16>, uint8_t, 10, 16>, 10, 16>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<2, 1>, uint8_t, 10, 1>, 10, 1>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<4, 1>, uint8_t, 10, 1>, 10, 1>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<8, 1>, uint8_t, 10, 1>, 10, 1>,
+        LayoutTraits<ccc::PodVector<ccc_test::Pod<16, 1>, uint8_t, 10, 1>, 10, 1>
+> LayoutTraitsTypes;
+INSTANTIATE_TYPED_TEST_CASE_P(PodVector, ConsistentLayoutTest, LayoutTraitsTypes);
